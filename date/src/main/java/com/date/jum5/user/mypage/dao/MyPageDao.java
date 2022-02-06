01f15ef21172bc92a11153608f76b7e564051b05
YoungYeonKim <97072412+YoungYeonKim@users.noreply.github.com>
2022-02-06 15:28:55 +0900

    Update MyPageDao.java

--- date/src/main/java/com/date/jum5/user/mypage/dao/MyPageDao.java
+++ date/src/main/java/com/date/jum5/user/mypage/dao/MyPageDao.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.date.jum5.user.login.vo.LoginVo;
+import com.date.jum5.user.mypage.vo.ProfileVo;
 
 @Repository
 public class MyPageDao {
@@ -14,12 +15,10 @@ public class MyPageDao {
 	@Autowired
 	SqlSession sqlSessionTemplate;
 	
-	LoginVo vo = new LoginVo();
 		
 		//마이페이지 테스트
-		public List<LoginVo> list(String id){
-			System.out.println(vo.getId());
-			return sqlSessionTemplate.selectList("list", id);
+		public List<LoginVo> mypageList(String id){
+			return sqlSessionTemplate.selectList("mypageList", id);
 			
 		}
 		
@@ -27,6 +26,30 @@ public class MyPageDao {
 		public int withdraw() {
 			// TODO Auto-generated method stub
 			return sqlSessionTemplate.update("withdraw");
+		}
+
+		public void insert(ProfileVo profileVo) {
+			// TODO Auto-generated method stub
+			sqlSessionTemplate.insert("insertProfile",profileVo);
+		}
+
+		public List<ProfileVo> profileList(String id) {
+			// TODO Auto-generated method stub
+			return sqlSessionTemplate.selectList("profileList",id);
+		}
+
+		public int update(LoginVo loginVo) {
+			return sqlSessionTemplate.update("update",loginVo);
+		}
+
+		public int delete(LoginVo loginVo) {
+			// TODO Auto-generated method stub
+			return sqlSessionTemplate.delete("delete",loginVo);
+		}
+
+		public int updateProfile(ProfileVo profileVo) {
+			// TODO Auto-generated method stub
+			return sqlSessionTemplate.update("updateProfile",profileVo);
 		}
 
 		
